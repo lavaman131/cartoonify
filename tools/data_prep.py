1e84903567d00714ae71457ce78958e4e78912b0
Yewon Lee <yewonlee@crc-dot1x-nat-10-239-15-241.bu.edu>
2024-04-14 18:08:49 -0400

    Adding dataset preparation file

---
+++ tools/data_prep.py
@@ -0,0 +1,18 @@
+from datasets import load_dataset
+from argparse import ArgumentParser
+
+def get_parser() -> ArgumentParser:
+    parser = ArgumentParser()
+    parser.add_argument("--data_dir", help="The local path to the image dataset.")
+    parser.add_argument("--repo_id", help="The name of the repo to push to Huggingface Hub.")
+    return parser
+
+def main() -> None:
+    parser = get_parser()
+    args = parser.parse_args()
+    dataset = load_dataset("imagefolder", data_dir=args.data_dir) 
+    dataset.push_to_hub(args.repo_id)
+
+
+if __name__ == "__main__":
+    main()
