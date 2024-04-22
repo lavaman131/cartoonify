c61e0b1b6473f883eaee36c269824033c99943af
lavaman131 <lavaalex3@gmail.com>
2024-04-22 19:08:58 +0000

    added revised training code

--- tools/push_model.sh
+++ tools/push_model.sh
@@ -2,10 +2,10 @@
 
 python push_model.py \
     --pretrained_model_name_or_path "stabilityai/stable-diffusion-2-1-base" \
-    --output_dir "/data/lora-dreambooth-model-v2/checkpoint-10000" \
-    --instance_prompt "disney style" \
+    --output_dir "/data/lora-dreambooth-model-v1/checkpoint-9000" \
+    --instance_prompt "disney" \
     --train_text_encoder \
     --repo_id "cartoonify" \
     --revision "main" \
     --prior_generation_precision "fp16" \
-    --commit_message "training after 10000 steps" \
+    --commit_message "training after 9000 steps" \
